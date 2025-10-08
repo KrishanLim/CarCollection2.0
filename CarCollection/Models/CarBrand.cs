a65e220883848aeabbf9962d0a3edd7629d5cd6e
Krishan Limbu <nikitlimbu006@gmail.com>
2025-10-07 21:16:30 -0400

    assignment 1

---
+++ CarCollection/Models/CarBrand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarCollection.Models
+{
+    public class CarBrand
+    {
+        public int CarBrandId { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public int FoundedYear { get; set; }
+        public ICollection<CarModel> CarModels { get; set; }
+    }
+}
---
+++ CarCollection/Models/CarModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CarCollection.Models
+{
+    public class CarModel
+    {
+        public int CarModelId { get; set; }
+        
+        public string ModelName { get; set; }
+
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+      
+        public int CarBrandId { get; set; }
+
+        public CarBrand CarBrand { get; set; }
+    }
+}
