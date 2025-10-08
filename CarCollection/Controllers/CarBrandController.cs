37835f01190018b4d62ae8e8bbfd75505edb3381
Krishan Limbu <nikitlimbu006@gmail.com>
2025-10-07 22:45:04 -0400

    added index links and run migration

--- CarCollection/Controllers/CarBrandController.cs
+++ CarCollection/Controllers/CarBrandController.cs
@@ -3,6 +3,7 @@ using CarCollection.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace CarCollection.Controllers
 {
     public class CarBrandController : Controller
---
+++ CarCollection/Data/Migrations/20251008023721_brandandreview.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarCollection.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class brandandreview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarBrands",
+                columns: table => new
+                {
+                    CarBrandId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FoundedYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarBrands", x => x.CarBrandId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarModels",
+                columns: table => new
+                {
+                    CarModelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ModelName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CarBrandId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarModels", x => x.CarModelId);
+                    table.ForeignKey(
+                        name: "FK_CarModels_CarBrands_CarBrandId",
+                        column: x => x.CarBrandId,
+                        principalTable: "CarBrands",
+                        principalColumn: "CarBrandId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarModels_CarBrandId",
+                table: "CarModels",
+                column: "CarBrandId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarModels");
+
+            migrationBuilder.DropTable(
+                name: "CarBrands");
+        }
+    }
+}
