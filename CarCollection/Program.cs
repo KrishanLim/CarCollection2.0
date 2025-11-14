733a30af8c54f3336156be5eb6ed5eeb950a29dc
Krishan Limbu <nikitlimbu006@gmail.com>
2025-11-14 05:49:37 -0500

    debugged some codes and created readme

--- CarCollection/Program.cs
+++ CarCollection/Program.cs
@@ -1,76 +1,85 @@
-using CarCollection.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+    using CarCollection.Data;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+
+    var builder = WebApplication.CreateBuilder(args);
 
 
-var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+
+    builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false; 
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+    builder.Services.AddRazorPages();
+    builder.Services.AddControllersWithViews();
 
 var configuration = builder.Configuration;
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false; 
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddRazorPages();
-builder.Services.AddControllersWithViews();
-
 builder.Services.AddAuthentication()
-    .AddGoogle(options =>
-    {
-        options.ClientId = configuration["Authentication:Google:ClientId"];
-        options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-    })
-    .AddGitHub(options =>
-    {
-        options.ClientId = configuration["Authentication:GitHub:ClientId"];
-        options.ClientSecret = configuration["Authentication:GitHub:ClientSecret"];
-    });
-
+        .AddGoogle(options =>
+        {
+            options.ClientId = configuration["Authentication:Google:ClientId"];
+            options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+        })
+        .AddGitHub(options =>
+        {
+            options.ClientId = configuration["Authentication:GitHub:ClientId"];
+            options.ClientSecret = configuration["Authentication:GitHub:ClientSecret"];
+        });
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    // Wait for async task in a simple way for beginners
+    SeedData.CreateInstructorAccountAsync(services).GetAwaiter().GetResult();
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-
-app.UseAuthentication();
-
-app.UseAuthorization();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
 
 
-app.MapStaticAssets();
+    app.UseAuthentication();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    app.UseAuthorization();
 
-app.MapRazorPages()
-   .WithStaticAssets();
 
-app.Run();
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.MapRazorPages()
+       .WithStaticAssets();
+
+    app.Run();
